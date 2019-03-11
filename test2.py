cafef3e661752dfbeb6ae58170223f0c97e39e1a
lAlexMel <48438959+lAlexMel@users.noreply.github.com>
2019-03-11 08:58:37 -0400

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,11 @@
+test2.py
+
+pip help
+pip install package_name
+pip uninstall package_name
+pip list
+pip show package_name
+pip search
+pip --proxy user:passwd@proxy.server:port
+pip install -U
+pip install --force-reinstall
